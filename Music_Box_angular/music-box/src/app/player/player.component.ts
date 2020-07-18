f707fb35167e10a350009375080b756411547e2c
starvvx <vaibhavvstar31@gmail.com>
2020-07-18 17:58:43 +0530

    main functionality added

--- Music_Box_angular/music-box/src/app/player/player.component.ts
+++ Music_Box_angular/music-box/src/app/player/player.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import {content} from '../constants';
 import { Router, ActivatedRoute, } from '@angular/router';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-player',
@@ -10,14 +11,53 @@ import { Router, ActivatedRoute, } from '@angular/router';
 export class PlayerComponent implements OnInit {
 
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    @Inject(DOCUMENT) document
   ) { }
   contentId:number;
   allContent = content;
   currContent:any;
+  song:HTMLAudioElement;
+  isplaying:boolean = false;
+  maxtime:number = 0;
+  elapsedtime:number = 0;
+  interval:any;
   ngOnInit(): void {
     this.contentId = this.route.snapshot.params['id'];
     this.currContent = this.allContent[this.contentId];
+    this.song = <HTMLAudioElement> document.getElementById('music');
+    this.interval = setInterval(this.timeCheck,600);
   }
-
-}
+  play()
+  {
+      if(!this.isplaying)
+      {
+          this.song.play();
+          this.isplaying = true;
+      }
+      this.maxtime++;
+  }
+  refresh(id:string) {
+    this.song.load();
+    this.isplaying = false;
+    this.maxtime = 0;
+    this.elapsedtime = 0;
+  }
+  timeCheck()
+  {
+      if(this.isplaying)
+      {
+          this.elapsedtime++;
+          if(this.elapsedtime == this.maxtime)
+          {
+              this.isplaying = false;
+              this.song.pause();
+          }
+      }
+      else if(this.elapsedtime < this.maxtime && !this.isplaying)
+      {
+          this.isplaying = true;
+          this.song.play();
+      }
+    }
+  }
